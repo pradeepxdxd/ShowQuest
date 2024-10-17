e476b573ffec8a3cbb8b130f93624eb36258eefa
pradeepxdxd <pradeepbiswas41813@gmail.com>
2024-10-17 14:34:34 +0530

    changes: drawer ui changes done

--- src/app/components/drawer/drawer.tsx
+++ src/app/components/drawer/drawer.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import List from "@mui/material/List";
-import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
@@ -13,6 +12,8 @@ import { Logout } from "@mui/icons-material";
 import { logout } from "@/app/store/auth/auth.slice";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "@/app/store";
+import { Avatar, Button, Divider, Grid, Typography } from "@mui/material";
+import { useRouter } from "next/navigation";
 
 type Anchor = "top" | "left" | "bottom" | "right";
 
@@ -29,49 +30,178 @@ const AnchorTemporaryDrawer: React.FC<DrawerProp> = ({
   toggleDrawer,
 }) => {
   const dispatch = useDispatch<AppDispatch>();
+  const router = useRouter();
+  const [auth] = React.useState(false);
 
   const handleLogout = () => {
     dispatch(logout());
   };
 
+  const handleEditProfile = () => {
+    router.push("/pages/profile/edit");
+  };
+
   const list = (anchor: Anchor) => (
     <Box
-      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 350 }}
       role="presentation"
       onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
     >
-      <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {["All mail", "Trash", "Spam", "Logout"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton onClick={handleLogout}>
-              <ListItemIcon>
-                {text === "Logout" ? (
-                  <Logout />
-                ) : index % 2 === 0 ? (
-                  <InboxIcon />
-                ) : (
-                  <MailIcon />
-                )}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+      {auth ? (
+        <>
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="flex-start"
+          >
+            <Box>
+              <ListItem>
+                <Typography variant="h5" fontWeight="bold">
+                  Hey!
+                </Typography>
+              </ListItem>
+              <ListItem>
+                <Typography
+                  variant="caption"
+                  fontWeight="normal"
+                  style={{ cursor: "pointer" }}
+                  onClick={handleEditProfile}
+                >
+                  Edit Profile {">"}
+                </Typography>
+              </ListItem>
+            </Box>
+            <Box>
+              <ListItem>
+                <Avatar
+                  alt="Cindy Baker"
+                  src="https://assets-in.bmscdn.com/static/2023/10/default-pic.png"
+                />
+              </ListItem>
+            </Box>
+          </Box>
+        </>
+      ) : (
+        <>
+          <Box>
+            <ListItem>
+              <Typography variant="h5" fontWeight={"bold"}>
+                Hey!
+              </Typography>
+            </ListItem>
+            <Divider />
+            <Grid container my={2}>
+              <Grid
+                item
+                sm={3}
+                display={"flex"}
+                justifyContent={"center"}
+                alignItems={"center"}
+              >
+                <Avatar
+                  src="https://in.bmscdn.com/webin/movies/superstar/rewards_login.png"
+                  alt="game"
+                />
+              </Grid>
+              <Grid item sm={6}>
+                <Typography variant="subtitle2">
+                  Unlock special offers and great benefits
+                </Typography>
+              </Grid>
+              <Grid item sm={3}>
+                <Button variant="outlined" color="error">
+                  Login
+                </Button>
+              </Grid>
+            </Grid>
+            <Divider />
+          </Box>
+        </>
+      )}
+
+      {!auth ? (
+        <>
+          <List>
+            {[
+              "Notifications",
+              "Play Credit Card",
+              "Help & Support",
+              "Rewards",
+              "BookAChange",
+            ].map((text, index) => (
+              <>
+                <ListItem key={text} disablePadding>
+                  <ListItemButton onClick={handleLogout}>
+                    <ListItemIcon>
+                      {text === "Logout" ? (
+                        <Logout />
+                      ) : index % 2 === 0 ? (
+                        <InboxIcon />
+                      ) : (
+                        <MailIcon />
+                      )}
+                    </ListItemIcon>
+                    <ListItemText primary={text} />
+                  </ListItemButton>
+                </ListItem>
+                <Divider />
+              </>
+            ))}
+          </List>
+        </>
+      ) : (
+        <>
+          <List>
+            {[
+              "Notificatons",
+              "Your Orders",
+              "Stream Library",
+              "Play Credit Card",
+              "Help Centre",
+              "Accounts & Settings",
+              "Rewards",
+              "Offers",
+              "Gift Cards",
+              "Food & Beverages",
+              "List Your Show",
+            ].map((text, index) => (
+              <>
+                <ListItem key={text} disablePadding>
+                  <ListItemButton>
+                    <ListItemIcon>
+                      {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                    </ListItemIcon>
+                    <ListItemText primary={text} />
+                  </ListItemButton>
+                </ListItem>
+                <Divider />
+              </>
+            ))}
+          </List>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Button
+              variant="outlined"
+              color="error"
+              sx={{
+                width: "80%",
+                position: "absolute",
+                bottom: "20px",
+                // bgcolor: "rgb(248, 68, 100)",
+              }}
+              onClick={handleLogout}
+            >
+              Sign Out
+            </Button>
+          </Box>
+        </>
+      )}
     </Box>
   );
 
