f5aecdb9fffc258f9ebb25b19cdcc34b25f10ab7
pradeepxdxd <pradeepbiswas41813@gmail.com>
2024-10-30 12:58:18 +0530

    fix : hotfix

--- src/app/components/modals/auth/otp/OtpModal.tsx
+++ src/app/components/modals/auth/otp/OtpModal.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useEffect, useState } from "react";
 import { Box, Button, Typography } from "@mui/material";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
