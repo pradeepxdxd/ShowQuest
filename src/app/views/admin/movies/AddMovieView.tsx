875d1f38a64d02fb9c0d624c92e57f079ea134ea
pradeepxdxd <pradeepbiswas41813@gmail.com>
2024-11-21 13:07:37 +0530

    fix production bug at add-show cant read property undefined read 0

--- src/app/views/admin/movies/AddMovieView.tsx
+++ src/app/views/admin/movies/AddMovieView.tsx
@@ -47,7 +47,7 @@ export default function AddMovieView() {
       <Container maxWidth="sm">
         <Typography variant="h6" textAlign="center" my={2}>
           Add{" "}
-          {id[0] === "movie"
+          {id && id != undefined && id[0] === "movie"
             ? "Movie"
             : id[0] === "live-event"
             ? "Live Event"
